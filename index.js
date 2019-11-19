b276093079e9e759ddd12b90f4868ebca0bfb62b
luisaga215 <luis.aga215@gmail.com>
2019-11-18 21:42:30 -0600

    Estructura

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ const express = require('express')
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/assets'));
-
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
 
-var server = app.listen(3000, function() {
-    console.log("Server in port 3000");
+var server = app.listen(8000, function() {
+    console.log("Server in port 8000");
 });
